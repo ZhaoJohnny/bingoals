c99c6be02f2258cc141b47a243fb8deb679b7968
Wesley Chen <wesleyc1223@gmail.com>
2026-06-16 20:54:56 -0700

    feat: added pool

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import pg from "pg";
 
 const app = express();
 const PORT = 3001;
+const { Pool } = pg;
 
 app.use(cors());
 app.use(express.json());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
 
 app.get('/', (req, res) => {
   res.send('Server is working');
