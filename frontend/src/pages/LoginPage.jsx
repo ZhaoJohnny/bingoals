0d4fe5cf935f8dca297c5eb261986b060b9b9572
whakley <wesleyc1223@gmail.com>
2026-07-06 19:11:43 -0700

    feat: added login api post

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,12 +2,31 @@ import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
     const navigate = useNavigate();
-
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const email = event.target[0].value;
+        const password = event.target[1].value;
+        try {
+            const response = await fetch("http://localhost:3001/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email:email, password:password })
+            });
+            if (response.ok) {
+            navigate("/start");
+        }
+        } catch (error) {
+            console.error("Error logging in:", error);
+        }
+        
+    }
   return (
     <div>
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
+      <form onSubmit={handleSubmit}>
+        
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
