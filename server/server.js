7b49d6ffb5e8a1e0cc19614e224c7939df2bf9d2
whakley <wesleyc1223@gmail.com>
2026-07-07 17:57:49 -0700

    fix: changed api/register password_hash to password for insert query

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.post("/api/register", async (req, res) => {
     const passwordHash = await bcrypt.hash(password, 10);
 
     const result = await pool.query(
-      `INSERT INTO users (name, email, password_hash)
+      `INSERT INTO users (name, email, password)
        VALUES ($1, $2, $3)
        RETURNING id, name, email, created_at`,
       [name, email, passwordHash]
