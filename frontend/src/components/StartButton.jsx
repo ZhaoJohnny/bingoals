5ad05ece4b97e32180901e603d3dcb6ecdc7e385
jzhao19 <jzhao19@students.cs.ubc.ca>
2026-07-16 22:45:21 -0700

    feat: added skeleton of start button

---
+++ frontend/src/components/StartButton.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import '../styles/StartButtonButton.css';
+
+function StartButton({playerID, boardID}) {
+    const [loading, setLoading] = useState(false);
+    async function handleClick() {
+    setLoading(true);
+    try {
+      const res = await fetch(`http://localhost:3001/api/board/${boardID}/start`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+         },
+        body: JSON.stringify({ user_id: playerID, board_id: boardID }),
+      });
+      const data = await res.json();
+      if (data.success) {
+        // TODO: Start the game
+      } else {
+        console.error('Failed to start ', data.message);
+      }
+    } catch (error) {
+      console.error('Error toggling ready', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <button className="start-button" onClick={handleClick} disabled={loading}>
+      Start Game
+    </button>
+  );
+}
+
+export default StartButton;
