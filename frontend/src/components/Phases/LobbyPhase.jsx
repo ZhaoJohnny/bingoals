711f64c8384a46038c1de295a1a26b2d0e865078
jzhao19 <jzhao19@students.cs.ubc.ca>
2026-07-16 18:58:57 -0700

    feat: changed api route

--- frontend/src/components/Phases/LobbyPhase.jsx
+++ frontend/src/components/Phases/LobbyPhase.jsx
@@ -2,14 +2,14 @@ import PlayerList from "../PlayerList";
 import ReadyButton from "../ReadyButton";
 import "../../styles/LobbyPhase.css";
 
-function LobbyPhase({ boardID, boardState, onToggle }) {
+function LobbyPhase({ boardID, playerID, onToggle }) {
   return (
     <div className="lobby-phase">
       <div className="lobby-center">
         <PlayerList boardID={boardID} />
       </div>
       <div className="lobby-bottom">
-        <ReadyButton boardState={boardState} onToggle={onToggle} />
+        <ReadyButton boardID={boardID} playerID={playerID} onToggle={onToggle} />
       </div>
     </div>
   );
